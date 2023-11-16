938cde46e2ad65b772631444133448a10f111c11
Fabian Perez <728710@student.dusd.net>
2023-11-16 15:31:55 -0800

    if stements
    I am almost done with if statements

---
+++ ScripptiingProjectPerez/Assets/Scripts/If statements scripts.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Ifstatementsscripts : MonoBehaviour
+{
+    float coffetemperature = 85.0f;
+    float hotLimitemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            TemperatureTest();
+
+        coffetemperature -= Time.deltaTime * 5f;
+    }
+    void TemperatureTest()
+    {
+
+        // If the coffee's temperature is greater than hottest drinking temperature...
+        if (coffetemperature < coldLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too hot.");
+        }
+        // If it  isn't, but the coffee temperature is less than the coldest drinking temperature...
+        else if (coffetemperature < coldLimitTemperature)
+
+            // ... do this.
+            print("Coffee is too cold.");
+        }
+        //  If it is  neither of those then...
+        else
+        {
+
+             // ... do this.
+             print("Coffee is just right.");
+}
+    
+
+
+
+
+
+
+
+
